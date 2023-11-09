add50abd00c61b4000e1d69463d5c428d982119c
Douglas Sousa <douglasmb87@gmail.com>
2023-11-09 11:32:45 -0300

    feat: add logic to pass question

--- src/app/pages/lp-question/lp-question.component.ts
+++ src/app/pages/lp-question/lp-question.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import QuestionAndAnswer from '../lp-welcome/lp-welcome-interface';
 
 @Component({
   selector: 'app-lp-question',
   templateUrl: './lp-question.component.html',
-  styleUrls: ['./lp-question.component.css']
+  styleUrls: ['./lp-question.component.css'],
 })
 export class LpQuestionComponent {
-
+  progressBar: number = 0;
+  questions: QuestionAndAnswer = JSON.parse(localStorage.getItem('question')!);
 }
